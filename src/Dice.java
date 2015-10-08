0482cf660a3e6c5bb86c415408a9fc8fb8df1b16
Ben Posey <brp76@pitt.edu>
2015-10-08 13:37:32 -0400

    Added counter method

--- src/Dice.java
+++ src/Dice.java
@@ -1,19 +1,6 @@
 import java.util.Random;
 
 public class Dice {
-	/*public int random;
-	
-	// Constructor
-	public Dice(){
-		Random rnd = new Random();
-		int random = rnd.nextInt(6)+1;
-	}
-	
-	// Accessor
-	public int getRoll(){
-		return random;
-	}*/
-	
 	public static int roll(int rollCount, Random r){
 		int randomSum = 0;
 		for (int i = 0; i < rollCount; i++){
--- src/Lab5.java
+++ src/Lab5.java
@@ -5,32 +5,46 @@ public class Lab5 {
 	public static int dice1 = 0, dice2 = 0;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int [] a = new int[12];
-
-		/*for (int i=0; i <100; i++){
-			int random = rnd.nextInt(6)+1;
-			System.out.println(val);
-		}*/
 		
-		/*
 		System.out.println("Number of rolls (100 or 1000)");
 		int rolls = sc.nextInt();
-
-		rollDice(rolls);*/
-		
-		
-		/**
-		 *New method
-		 */
 		
 		Random r = new Random();
-		Dice.roll(2,r);
-		System.out.println(Dice.roll(2, r));
-
+		rollDice(rolls,r);
 	}
-	//rollDice(int n)
-	//Dice _dice1 = new Dice();
-	//Dice _dice2 = new Dice();
-	//System.out.println(_dice1.getRoll());
+	
+	public static void rollDice(int n, Random r){
+		int [] a = new int[12];
+		for (int i=0; i < n; i++){
+			int roll = Dice.roll(2,r);
+			//System.out.println(roll);
+			if (roll == 2) {
+	    	    a[0] ++;
+	    	} else if (roll == 3) {
+	    		a[1] ++;
+	    	} else if (roll == 4) {
+	    		a[2] ++;
+	    	} else if (roll == 5) {
+	    		a[3] ++;
+	    	} else if (roll == 6) {
+	    		a[4] ++;
+	    	} else if (roll == 7) {
+	    		a[5] ++;
+	    	} else if (roll == 8) {
+	    		a[6] ++;
+	    	} else if (roll == 9) {
+	    		a[7] ++;
+	    	} else if (roll == 10) {
+	    		a[8] ++;
+	    	} else if (roll == 11) {
+	    		a[9] ++;
+	    	} else if (roll == 12) {
+	    		a[10] ++;
+	    	}
+		}
+		for (int j = 0; j <= 10; j++){
+			System.out.printf("%d was rolled %d out of %d times\n",j+2, a[j], n);
+		}
+	}
 
 }
