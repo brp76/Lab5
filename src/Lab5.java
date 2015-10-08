778e5b23f816b0d38a1482948cf59b2c909075a9
Ben Posey <brp76@pitt.edu>
2015-10-08 12:10:56 -0400

    Intial commit

---
+++ src/Lab5.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Lab5 {
+	public static int dice1 = 0, dice2 = 0;
+	public static void main(String[] args) {
+		Random rnd = new Random();
+		long seed = 0;
+		rnd.setSeed(seed);
+		System.out.println(rnd);
+	}
+	public static void rollDice(int n, Random rnd){
+		
+	}
+}
